60b6aa3a4fa74603667c80717ef04045da354c36
Luka <luka.halzer@leriha.si>
2025-03-12 01:06:56 +0100

    added postgre container

--- server.js
+++ server.js
@@ -1,8 +1,35 @@
 const express = require('express');
 const path = require('path');
+const { Client } = require("pg");
 
 const app = express();
 const port = 3000;
+
+const client = new Client({
+
+  host: 'localhost',
+  port: 5432,
+  user: 'admin',
+  password: 'secret',
+  database: 'shelly'
+
+
+});
+
+client.connect()
+  .then(() => {
+
+    console.log("connected to db")
+
+    return client.query('SELECT table_name FROM information_schema.tables WHERE table_schema = "public";')
+
+  })
+
+  .catch(err => {
+    console.error(err.stack)
+
+  })
+
 
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -12,12 +39,20 @@ app.get('/', (req, res) => {
 
 app.get('/status', async (req, res) => {
 
-    const response = await fetch("http://192.168.1.243/rpc/Switch.GetStatus?id=0");
-    const data = await response.json()
-    console.log(data)
+      const response = await fetch("http://192.168.1.243/rpc/Switch.GetStatus?id=0");
+    const data = await response.json();
+
+
+    const tarifaRes = await fetch("https://api-drzavno-test.scv.si/api/tarifa");
+    const tarifa = await tarifaRes.json();
+    console.log(tarifa.tarifa)
+
+    //formula za trenutno porabo
 
   res.send(data);
 });
+
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
