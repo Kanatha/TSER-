3e2bef0b179ca94a081062b9c288a9bc6a3a4d54
Luka <luka.halzer@leriha.si>
2025-03-24 21:59:39 +0100

    done

--- server.js
+++ server.js
@@ -1,59 +1,67 @@
-const express = require('express');
-const path = require('path');
+const express = require("express");
 const { Client } = require("pg");
 
 const app = express();
 const port = 3000;
 
 const client = new Client({
-
-  host: 'localhost',
+  host: "localhost",
   port: 5432,
-  user: 'admin',
-  password: 'secret',
-  database: 'shelly'
-
-
+  user: "admin",
+  password: "secret",
+  database: "shelly",
 });
 
-client.connect()
-  .then(() => {
+client.connect();
 
-    console.log("connected to db")
+const readTable = async () => {
+  const res = await client.query(
+    "SELECT * FROM tarifa ORDER BY id desc LIMIT 100"
+  );
 
-    return client.query('SELECT table_name FROM information_schema.tables WHERE table_schema = "public";')
+  return res.rows;
+};
 
-  })
+const writeTable = async (power, tarif) => {
+  await client.query("INSERT INTO tarifa(tarifa, moc) VALUES($1, $2)", [
+    tarif,
+    power,
+  ]);
+};
 
-  .catch(err => {
-    console.error(err.stack)
+const getData = async () => {
+  const tarifaRes = await fetch("https://api-drzavno-test.scv.si/api/tarifa");
+  const tarifa = await tarifaRes.json();
 
-  })
+  const powerRes = await fetch(
+    "http://192.168.1.243/rpc/Switch.GetStatus?id=0"
+  );
+  const power = await powerRes.json();
 
+  //console.log({ power, tarifa });
+  //console.log(tarifa.tarifa, power.apower);
+  return [tarifa.tarifa, power.apower];
+};
 
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+app.get("/status", async (req, res) => {
+  const data = await getData();
+  res.json(data[1]);
 });
 
-app.get('/status', async (req, res) => {
+setInterval(async () => {
+  const data = await getData();
 
-      const response = await fetch("http://192.168.1.243/rpc/Switch.GetStatus?id=0");
-    const data = await response.json();
+  writeTable(data[1], data[0]);
+}, 5000);
 
-
-    const tarifaRes = await fetch("https://api-drzavno-test.scv.si/api/tarifa");
-    const tarifa = await tarifaRes.json();
-    console.log(tarifa.tarifa)
-
-    //formula za trenutno porabo
-
-  res.send(data);
+app.get("/data", async (req, res) => {
+  const tableData = await readTable();
+  console.log(tableData);
+  res.json(tableData);
 });
-
-
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
+
+//TODO: Set interval on writeTable to write every 5 seconds instead on on request.
